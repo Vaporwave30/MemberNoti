0ea652f0a7c96c3a7f516beb7106f4b2bacd768a
Vaporwave30 <43111111+Vaporwave30@users.noreply.github.com>
2018-12-02 15:58:59 +0200

    Update webhook.py
    json json json json json json json json json json

--- webhook.py
+++ webhook.py
@@ -1,1 +1,42 @@
 import requests
+def post_data(url, desc, user, link, UID):
+    post = {
+        "username": "User notifier!",
+        "avatar_url": "https://i.boring.host/J66Eot2.png",
+        "embeds": [
+        {
+            "author": {
+            "name": "Vaporwave30",
+            "url": "https://v3rmillion.net/member.php?action=profile&uid=393343",
+            "icon_url": "https://i.boring.host/J679G0m.png"
+        },
+            "title": "Another new user has signed up to this site!",
+            "description": desc,
+            "color": 3843043,
+            "fields": [
+            {
+                "name": "Username:",
+                "value": user,
+                "inline": True
+            },
+            {
+                "name": "Link:",
+                "value": link,
+                "inline": True
+            },
+            {
+                "name": "UserID:",
+                "value": UID,
+                "inline": False
+            },
+        ],
+            "footer": {
+             "text": "Made by Vaporwave30 ",
+                "icon_url": "https://i.boring.host/JDZiDf5.png"
+            }
+        }
+        ]
+    }
+    a = requests.post(url, json=post)
+    print(a.status_code)
+
